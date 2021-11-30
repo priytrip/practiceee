a96f165fa2e4be0656e5c3539ba76a6f8d9bba07
Priya Tripathi <priya.tripathi@coupa.com>
2021-11-30 20:32:12 +0530

    rtest

---
+++ shoes/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'users/index'
+  post '/sessions' => 'sessions#create'
+  post '/users' => 'users#create'
+  get '/dashboard' => 'shoes#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Almost every application defines a route for the root path ("/") at the top of this file.
+  # root "articles#index"
+end
